a40c08f20575bcf9aa8ad776039a74b6f05282b0
kewatvinod9936 <159791676+kewatvinod9936@users.noreply.github.com>
2024-02-17 14:52:25 +0530

    summernote
    summernote use in add post

---
+++ inc/sidebar.php
@@ -0,0 +1,37 @@
+        <div class="col-md-3">
+            <ul class="list-group">
+             <a href="index.php" class="list-group-item active">
+                <i class="fa fa-tachometer"></i> 
+                 Dashboard
+             </a>
+             <a href="posts.php" class="list-group-item">
+                 <span class="badge">14</span>
+                 <i class="fa fa-file-text-o"></i> 
+                 All Posts
+             </a>
+             
+             <a href="media.php" class="list-group-item">
+                 <span class="badge">14</span>
+                 <i class="fa fa-database"></i> 
+                 Media
+             </a>
+              
+             <a href="comments.php" class="list-group-item">
+                 <span class="badge">18</span> 
+                 <i class="fa fa-comment"></i> 
+                 Comments
+             </a>
+             
+              <a href="categories.php" class="list-group-item">
+                 <span class="badge">1</span>
+                 <i class="fa fa-folder-open"></i> 
+                 Categories
+             </a>
+             
+              <a href="users.php" class="list-group-item">
+                 <span class="badge">8</span>
+                 <i class="fa fa-users"></i> 
+                 Users
+             </a> 
+            </ul>
+        </div>
